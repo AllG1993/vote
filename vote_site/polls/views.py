173e3cc1c643015153cfd4d00c3e2bbc67f9a2cf
Aleksey G <alekseygalkovich@gmail.com>
2021-07-23 16:29:26 +0300

    Start Vote 3

--- vote_site/polls/views.py
+++ vote_site/polls/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/index.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse(f'Вы смотрите на результаты вопроса {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Вы голосуете по вопросу {question_id}')
+
